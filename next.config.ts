f0cbffe0cb2a0171aab5aa7a82d2a7f08f2021ec
Sai Ram Chikkala <sairamch10@gmail.com>
2026-07-21 23:05:05 +0530

    update/fix/builderror

--- next.config.ts
+++ next.config.ts
@@ -148,6 +148,15 @@ const nextConfig: NextConfig = {
 			},
 		},
 	},
+  async rewrites() {
+    const backendUrl = process.env.AUTH_API_URL || "http://localhost:8080";
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${backendUrl}/api/v1/:path*`,
+      },
+    ];
+  },
   async headers() {
     return [
       {
